43c49469d8fd17309a45c2eb50341cb189eb70f4
Gustav Zander <gustav.zander@nordnet.se>
2018-05-16 15:30:27 +0200

    Components fetch data instead of App

--- src/components/newsItem/index.js
+++ src/components/newsItem/index.js
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { distanceInWordsToNow } from 'date-fns';
 import './styles.css'
+import { api } from '../../utils';
 
-export const NewsItem = ({item}) => {
+class NewsItem extends Component {
+  constructor(props) {
+    super(props);
 
-  if (item === undefined) {
-    return <div />
+    this.state = {
+      item: {},
+    }
   }
-  const actualTime = item.time * 1000
-  return (
-    <div className="item-container">
-      <a className="title" href={item.url}>
-        {item.title}
-      </a>
-      <br />
-      <Link to={`/item/${item.id}`}>
-        {item.score} points 
+
+  componentDidMount() {
+    const { id } = this.props
+    api.getItem(id)
+      .then(itemResponse => {
+        this.setState(
+          {
+            item: itemResponse
+          }
+        )
+      })
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state.item !== nextState.item
+  }
+
+  render() {
+    const { item } = this.state
+
+    if (item === undefined) {
+      return <div />
+    }
+    const actualTime = item.time * 1000
+    return (
+      <div className="item-container">
+        <a className="title" href={item.url}>
+          {item.title}
+        </a>
+        <br />
+        <Link to={`/item/${item.id}`}>
+          {item.score} points
       </Link>
-      <span> by {item.by} {<time dateTime={new Date(actualTime)}>{distanceInWordsToNow(new Date(actualTime))}</time>} ago | </span>
-      <Link to={`/item/${item.id}`}>
-      { item.descendants === 0
-        ? 'discuss'
-        : item.descendants + ' comments'
-      }
-      </Link>
-    </div>
-  )
+        <span> by {item.by} {<time dateTime={new Date(actualTime)}>{distanceInWordsToNow(new Date(actualTime))}</time>} ago | </span>
+        <Link to={`/item/${item.id}`}>
+          {item.descendants === 0
+            ? 'discuss'
+            : item.descendants + ' comments'
+          }
+        </Link>
+      </div>
+    )
+  }
 }
+
+export { NewsItem };
--- src/components/newsItemList/index.js
+++ src/components/newsItemList/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { NewsItem } from '../newsItem';
 import './styles.css'
 
-export const NewsItemList = ({ items, refresh }) => (
-  <div>
-   <button onClick={refresh}>Refresh</button>
-     <ol>
-      {Object.values(items).map(item => (<li key={item.id}><NewsItem item={item} /></li>))}
-    </ol>
-  </div>
+export const NewsItemList = ({ ids }) => (
+  <ol>
+    {ids.map(id => (<li key={id}><NewsItem id={id} /></li>))}
+  </ol>
 )
